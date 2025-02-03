cd62fd77981ccdc37e73f2e35608bb5bd805063d
dioauliaharrisa <daharrisa@gmail.com>
2025-02-04 00:59:55 +0700

    Add function to determine MIME type from buffer in DailyReport

--- src/pages/daily/index.tsx
+++ src/pages/daily/index.tsx
@@ -33,12 +33,39 @@ export const DailyReport = () => {
     fetchReports();
   }, []);
 
+  function getMimeTypeFromMagicNumber(buffer) {
+    const uint = new Uint8Array(buffer).subarray(0, 4);
+    const header = uint.reduce(
+      (str, byte) => str + byte.toString(16).padStart(2, "0"),
+      ""
+    );
+
+    const signatures = {
+      "89504e47": "image/png", // PNG
+      ffd8ffe0: "image/jpeg", // JPEG
+      ffd8ffe1: "image/jpeg", // JPEG
+      ffd8ffe2: "image/jpeg", // JPEG
+      ffd8ffe3: "image/jpeg", // JPEG
+      ffd8ffe8: "image/jpeg", // JPEG
+      "47494638": "image/gif", // GIF
+      "49492a00": "image/tiff", // TIFF (little endian)
+      "4d4d002a": "image/tiff", // TIFF (big endian)
+      "52494646": "image/webp", // WebP
+      d0cf11e0a1b11ae1: "application/vnd.ms-excel", // XLS (Binary)
+      "504b0304": "application/zip", // XLSX (needs further checking)
+    };
+
+    return signatures[header] || "application/octet-stream";
+  }
+
   const rows = reports.map((report) => {
     const bufferData = new Uint8Array(report.documentation);
+    const mimeType = getMimeTypeFromMagicNumber(bufferData);
     const blob = new Blob([bufferData], { type: "application/zip" });
     const file = new File([blob], "your-file-name.zip", {
       type: "application/zip",
     });
+    console.log("🦆 ~ rows ~ file:", file);
     return (
       <Table.Tr key={report.id}>
         <Table.Td>
@@ -52,7 +79,7 @@ export const DailyReport = () => {
         <Table.Td>{report.report.type ?? "-"}</Table.Td>
         <Table.Td>{report.personnels}</Table.Td>
         <Table.Td>{report.report.outputReport}</Table.Td>
-        <Table.Td>{report.documentation ? file : "No"}</Table.Td>
+        <Table.Td>{report.documentation ? "file" : "No"}</Table.Td>
       </Table.Tr>
     );
   });
@@ -70,7 +97,7 @@ export const DailyReport = () => {
       <Paper className={styles.content}>
         <Group justify="space-between">
           <Text className={styles.title}>Laporan Harian</Text>
-          <Button onClick={() => toggleForm()}>Tombol</Button>
+          <Button onClick={() => toggleForm()}>Buat laporan</Button>
         </Group>
         <Table className={styles.table}>
           <Table.Thead className={styles.table_head}>
