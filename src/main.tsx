a83dc7ce32d357c67065c0f0e856d5aa32070847
dioauliaharrisa <daharrisa@gmail.com>
2025-02-11 07:56:51 +0700

    Refactor App component structure for router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+
+import { theme } from "./theme";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Layout } from "./pages/layout";
+import { MonthlyReport } from "./pages/monthly";
+import { DailyReport } from "./pages/daily";
+import { Dashboard } from "./pages/dashboard";
+import { Login } from "./pages/login";
+import ProtectedRoute from "./pages/components/protected-route";
+import { AuthProvider } from "./pages/components/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+            children: [],
+          },
+          {
+            path: "/daily",
+            element: <DailyReport />,
+            children: [],
+          },
+          {
+            path: "/monthly",
+            element: <MonthlyReport />,
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </MantineProvider>
   </React.StrictMode>
 );
